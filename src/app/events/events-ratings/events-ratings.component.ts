234ce1a20ec3bf30ffcb1498719377d8eb237645
Patric França <patric.franca@hubfintech.com.br>
2018-08-16 10:24:54 -0300

    implementando o form de ratings e criando o form

--- src/app/events/events-ratings/events-ratings.component.ts
+++ src/app/events/events-ratings/events-ratings.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-events-ratings',
@@ -8,13 +9,37 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class EventsRatingsComponent implements OnInit {
 
-  constructor(private modalService: NgbModal) { }
+  ratingsForm: FormGroup;
+
+  constructor(
+    private modalService: NgbModal,
+    private formBuilder: FormBuilder
+  ) {
+    this.ratingsForm = this.createRatingForm();
+  }
 
   ngOnInit() {
   }
 
   openModal(content) {
     this.modalService.open(content, { size: 'lg' });
+  }
+
+  createRatingForm() {
+    return this.formBuilder.group({
+      ratings: this.formBuilder.group({
+        food: [null],
+        pub: [null],
+        local: [null],
+        bathroom: [null],
+        security: [null],
+        lighting: [null],
+        lineup: [null],
+        stall: [null],
+        cleaning: [null]
+      }),
+      comment: [null]
+    });
   }
 
 }

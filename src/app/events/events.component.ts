4b998b2372e947622ed6b77f7df5386d78d202e2
Patric Pereira <patric.franca@hubfintech.com.br>
2018-08-30 18:42:44 -0300

    alteração template events

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import * as anime from 'animejs';
 
 import { EventsService } from '../providers/events.service';
 import { Event } from '../shared/models/event.interface';
@@ -11,6 +12,8 @@ import { Event } from '../shared/models/event.interface';
 export class EventsComponent implements OnInit {
 
   public events: Event[];
+  @ViewChild('card') card;
+  public playing = false;
 
   constructor(
     private eventsService: EventsService
@@ -25,6 +28,26 @@ export class EventsComponent implements OnInit {
       .subscribe((events: Event[]) => {
         this.events = events;
       });
+  }
+
+  public flipCard() {
+    console.log(this.card);
+    if (this.playing) {
+      return;
+    }
+
+    this.playing = true;
+    anime({
+      targets: this.card.nativeElement,
+      scale: [{value: 1}, {value: 1}, {value: 1, delay: 250}],
+      rotateY: {value: '+=180', delay: 200},
+      easing: 'easeInOutSine',
+      duration: 400,
+      complete: () => {
+        this.playing = false;
+      }
+    });
+
   }
 
 }

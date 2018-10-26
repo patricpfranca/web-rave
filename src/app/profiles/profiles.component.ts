3e06e56c0cf323a8e7c2560bbef9067885b5b97a
Patric Pereira <patric.franca@hubfintech.com.br>
2018-10-26 14:36:16 -0300

    buscando os dados de profile

--- src/app/profiles/profiles.component.ts
+++ src/app/profiles/profiles.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { User } from '../shared/models/user.model';
+import { UserService } from '../providers/user.service';
 
 @Component({
   selector: 'app-profiles',
@@ -8,9 +10,23 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class ProfilesComponent implements OnInit {
 
-  constructor() { }
+  public profile: User[] = [];
+  private currentUser = JSON.parse(sessionStorage.getItem('currentUser'));
+
+  constructor(
+    private userService: UserService
+  ) { }
 
   ngOnInit() {
+    this.getProfile(this.currentUser._id);
+  }
+
+  public getProfile(id: string) {
+    this.userService.getById(id)
+      .subscribe((profile: User[]) => {
+        console.log(profile);
+        this.profile = profile;
+      });
   }
 
 }
